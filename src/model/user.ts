c39e65a8b81a89e9656102274f3904e3d869c885
Melika Paktalat <paktalatmelika@gmail.com>
2025-12-05 22:04:32 +0330

    add user model

---
+++ src/model/user.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+const userEnum = {
+    FEMALE: 'female',
+    MALE: 'male'
+}
+interface IUser {
+    firstName: string;
+    lastName: string;
+    gender?: string;
+    contactId: string;
+}
+
+const userSchema = new Schema<IUser>({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: userEnum
+    },
+    contactId: {
+        type: String,
+        required: true
+    }
+},
+    {
+        timestamps: true
+    })
+
+export const User = model<IUser>('user', userSchema);
