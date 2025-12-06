0c8c3591ea50f778ac38ae955656b22b1be76d86
melika_pt <m.paktalat@mail.pic>
2025-12-06 10:23:48 +0330

    add validate middleware

---
+++ src/middleware/validateData.ts
@@ -0,0 +1,20 @@
+import {ZodObject,ZodError} from "zod";
+import {Request,Response,NextFunction} from "express";
+
+export const validate = (schema:ZodObject)=>{
+    async(req:Request,res:Response,next:NextFunction)=>{
+        try {
+            await schema.parseAsync({
+                body:req.body,
+                query:req.query,
+                params:req.params
+            })
+            next()
+        } catch (error) {
+            if(error instanceof ZodError){
+                return res.status(400).json({cause:error.cause,message:error.message})
+            }
+            return res.status(500).json('Internal server error...')
+        }
+    }
+}
