119f110727e0d2efae06ed7ffe90eb93899c461a
melika_pt <m.paktalat@mail.pic>
2025-12-08 16:21:03 +0330

    fix validateData middleware

--- src/middleware/validateData.ts
+++ src/middleware/validateData.ts
@@ -2,12 +2,12 @@ import {ZodObject,ZodError} from "zod";
 import {Request,Response,NextFunction} from "express";
 
 export const validate = (schema:ZodObject)=>{
-    return async(req:Request,res:Response,next:NextFunction)=>{
+    return (req:Request,res:Response,next:NextFunction)=>{
         try {
-            await schema.parseAsync({
+             schema.safeParse({
                 body:req.body,
-                query:req.query,
-                params:req.params
+                // query:req.query,
+                // params:req.params
             })
             next()
         } catch (error) {
